47a52650433b1ca8e88803ba9824d01bf4b2bf13
angelo <angelo.hizon27@gmail.com>
2022-06-20 15:06:06 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading.jsx";
+import UnorderedList from "./UnoderedList.jsx";
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <UnorderedList />
+  </div>,
+  document.getElementById("root")
+);
